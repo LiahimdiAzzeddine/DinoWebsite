083017c0027504894e30464e59068ff3c987873d
oriwamienai24 <oriwamienai24@gmail.com>
2025-06-05 15:25:33 +0100

    fix dark background color issue.

--- src/components/experience/MODEL_CONFIGS.jsx
+++ src/components/experience/MODEL_CONFIGS.jsx
@@ -11,7 +11,7 @@ const MODEL_CONFIGS = {
     colorA: '#d7e0eb', 
     colorB: '#cc64ca',
     Component: Web1,
-  }, 
+  },
   Model2: {
     url: "./models/web2.glb",
     triggerSection: "#section3",
@@ -27,7 +27,34 @@ const MODEL_CONFIGS = {
     colorA: '#d7e0eb', 
     colorB: '#cc64ca',
     Component: Web3,
-  }
+  },
+  // I added this to fix a lightning change, I found that a lot of places reference Model1 explicitly.
+  // I changed model ids to be web1, web2, etc, and so I added this since some lighting and scene coloring files use it to get lighting data.
+  // I also left Model1, Model2, etc, since they are still referenced all around, and I felt lazy to change them all. sorry about the mess.
+  web1: {
+    url: "./models/web1.glb",
+    triggerSection: "#section2",
+    sectioncolor: 0x9a70e7,
+    colorA: '#d7e0eb',
+    colorB: '#cc64ca',
+    Component: Web1,
+  },
+  web2: {
+    url: "./models/web2.glb",
+    triggerSection: "#section3",
+    sectioncolor: 0x9a70e7,
+    colorA: '#d7e0eb',
+    colorB: '#cc64ca',
+    Component: Web2,
+  },
+  web3: {
+    url: "./models/web3.glb",
+    triggerSection: "#section4",
+    sectioncolor: 0x9a70e7,
+    colorA: '#d7e0eb',
+    colorB: '#cc64ca',
+    Component: Web3,
+  },
 
 };
 

b929734c7333a328ba93e0428fae502b27287ed4
Stifffller <100485595+Stifffller@users.noreply.github.com>
2025-07-21 19:14:38 +0100

    commit

--- src/components/experience/MODEL_CONFIGS.jsx
+++ src/components/experience/MODEL_CONFIGS.jsx
@@ -4,9 +4,9 @@ const MODEL_CONFIGS = {
   web1: {
     url: "./models/web1.glb",
     triggerSection: "#section2",
-   sectioncolor: 0x14b8a6, // teal-500 (#14b8a6)
-    colorA: '#d7e0eb',
-    colorB: '#6ac0f1',
+     sectioncolor: 0x6ac0f1, // teal-500 (#14b8a6)
+    colorB: '#9ce1dd',
+    colorA: '#9ce1dd',
   },
   web2: {
     url: "./models/web2.glb",
@@ -18,9 +18,9 @@ const MODEL_CONFIGS = {
   web3: {
     url: "./models/web3.glb",
     triggerSection: "#section4",
-  sectioncolor: 0x14b8a6, // teal-500 (#14b8a6)
-    colorA: '#d7e0eb',
-    colorB: '#6ac0f1',               // Teal-500 (plus saturé et profond)
+    sectioncolor: 0x2dd4bf, // teal-400 (#2dd4bf)
+    colorA: '#e6fcf5',                    // Très clair pastel vert/teal (plus clair que emerald-50)
+    colorB: '#6ac0f1',                    // Teal-500 (plus saturé et profond)
   },
 };
 
